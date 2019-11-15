83dd4dd8019ef970648e2572e1551bb83a40af7a
michamichael <kogan-misha@rambler.ru>
2019-11-15 01:15:21 -0600

    Implemented hh.ru api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.hh.ru/vacancies'
+    @uri = URI(@url)
+    @responce = Net::HTTP.get(@uri)
+    @jobs = JSON.parse(@responce)
   end
+
+
 
   def already_cv_sent
   end
