40cb3c3e162c510985d432787f553f9c88a36d78
Michael Kogan <mike@sliday.com>
2020-08-23 15:18:33 +0300

    moved parsing to separate method with responce checking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,23 @@
+require 'net/http'
+require 'json'
+
 class HomeController < ApplicationController
   def index
-    require 'net/http'
-    require 'json'
+    @jobs = parse_hh
+  end
 
+  private
+
+  def parse_hh
     url = 'https://api.hh.ru/vacancies?text=ruby&order_by=relevance&search_period=&per_page=100'
-    html = URI(url)
-
+    html = URI.parse(url)
     responce = Net::HTTP.get(html)
-    @jobs = JSON.parse(responce)
+
+    if response.code == '200'
+      JSON.parse(responce)
+    else
+      puts "Error"
+      puts "Response code: #{responce.code}"
+    end
   end
 end
